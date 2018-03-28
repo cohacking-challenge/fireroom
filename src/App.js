0adebc614a2f7185aad4737faa953cf36d1047f6
Yacine Hmito <yacine.hmito@gmail.com>
2018-03-28 20:43:22 +0200

    Setup react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  NavLink,
+} from 'react-router-dom';
+import Signup from './components/Signup';
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
 
 class App extends Component {
   constructor(props) {
@@ -16,10 +29,26 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. Hello{' '}
-          {this.state.name}
-        </p>
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <NavLink exact to="/" activeStyle={{ fontWeight: 'bold' }}>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/signup" activeStyle={{ fontWeight: 'bold' }}>
+                  Signup
+                </NavLink>
+              </li>
+            </ul>
+
+            <hr />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={Signup} />
+          </div>
+        </Router>
       </div>
     );
   }
---
+++ src/components/Signup.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <p>Signup and set the room on Fire!</p>;
+  }
+}
+export default Signup;
